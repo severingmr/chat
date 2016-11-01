b424e495e76fc8e652c99d5a911ea16856953ca9
severingmr <sevi36@hotmail.com>
2016-11-01 20:33:23 +0100

    change html

--- source/php/classes/ChatUser.class.php
+++ source/php/classes/ChatUser.class.php
@@ -19,7 +19,7 @@ class ChatUser extends ChatBase{
 	public function registriern()
     {
         DB::query("
-			INSERT INTO users (name,email, status)
+			INSERT INTO user (name,email, status)
 			VALUES (
 				'".DB::esc($this->name)."',
 				'".DB::esc($this->email)."',
