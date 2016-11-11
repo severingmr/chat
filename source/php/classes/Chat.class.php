aa10840047a602a650ffd50b24faade97bebad2b
severingmr <sevi36@hotmail.com>
2016-11-11 19:31:54 +0100

    change html

--- source/php/classes/Chat.class.php
+++ source/php/classes/Chat.class.php
@@ -260,8 +260,8 @@ class Chat
 
     public static function isAllowed($name, $email)
     {
-        $stmt = DB::query("SELECT COUNT(*) FROM user WHERE email='peter@meier.ch' AND name='peter' AND status='ok'");
-        $count = $stmt->fetch_object()[0];
+        $stmt = DB::query("SELECT COUNT(*) AS cnt FROM user WHERE email='peter@meier.ch' AND name='peter' AND status='ok'");
+        $count = $stmt->fetch_object()->cnt;
         return ($count > 0);
     }
 
