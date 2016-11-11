6abc41ccc7be79b269902feeb5e232a3c7f13355
severingmr <sevi36@hotmail.com>
2016-11-11 17:42:39 +0100

    change html

--- source/php/classes/Chat.class.php
+++ source/php/classes/Chat.class.php
@@ -15,7 +15,8 @@ class Chat
             throw new Exception('Your email is invalid.');
         }
 
-        return self::isAllowed($name, $email);
+        return "hallo";
+            //self::isAllowed($name, $email);
 
         if (!self::isAllowed($name, $email))
         {
