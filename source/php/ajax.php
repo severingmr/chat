788b56b771a21522af29fac4154248119ff082ac
severingmr <sevi36@hotmail.com>
2016-11-14 20:53:04 +0100

    without logs

--- source/php/ajax.php
+++ source/php/ajax.php
@@ -3,10 +3,10 @@
 /* Database Configuration. Add your details below */
 
 $dbOptions = array(
-	'db_host' => 'localhost',
-	'db_user' => 'aitec',
-	'db_pass' => 'dachs',
-	'db_name' => 'chat'
+    'db_host' => 'localhost',
+    'db_user' => 'aitec',
+    'db_pass' => 'dachs',
+    'db_name' => 'chat'
 );
 
 /* Database Config End */
@@ -23,54 +23,65 @@ require "classes/ChatUser.class.php";
 session_name('webchat');
 session_start();
 
-try{
-	
-	// Connecting to the database
-	DB::init($dbOptions);
-	
-	$response = array();
-	
-	// Handling the supported actions:
-	
-	switch($_GET['action']){
-		
-		case 'login':
-        //$username = checkInput($_Post['name']);
-        //$passwort = checkInput($_Post['passwort']);
+try {
 
-        //$response = Chat::login($username, $passwort);
+    // Connecting to the database
+    DB::init($dbOptions);
 
-        $response = Chat::login($_POST['name'],$_POST['email']);
-        break;
+    $response = array();
+
+    // Handling the supported actions:
+
+    switch ($_GET['action']) {
+
+        case 'login':
+            $username = checkInput($_Post['name']);
+            $email = checkInput($_Post['email']);
+
+            $response = Chat::login($username, $email);
+
+            //$response = Chat::login($_POST['name'],$_POST['email']);
+            break;
 
         case 'register':
+            $username = checkInput($_Post['name']);
+            $email = checkInput($_Post['email']);
 
-            $response = Chat::register($_POST['name'],$_POST['email']);
+            $response = Chat::register($username, $email);
+
+            //$response = Chat::register($_POST['name'], $_POST['email']);
 
             break;
-		
-		case 'checkLogged':
-			$response = Chat::checkLogged();
-		break;
-		
-		case 'logout':
-			$response = Chat::logout();
-		break;
-		
-		case 'submitChat':
-			$response = Chat::submitChat($_POST['chatText']);
-		break;
-		
-		case 'getUsers':
-			$response = Chat::getUsers();
-		break;
-		
-		case 'getChats':
-			$response = Chat::getChats($_GET['lastID']);
-		break;
 
-		case 'admin':
-			$response = Chat::loginAdmin($_POST['name'],$_POST['email']);
+        case 'checkLogged':
+            $response = Chat::checkLogged();
+            break;
+
+        case 'logout':
+            $response = Chat::logout();
+            break;
+
+        case 'submitChat':
+            $r_chatText = checkInput($_POST['chatText']);
+
+            $response = Chat::submitChat($r_chatText);
+            break;
+
+        case 'getUsers':
+            $response = Chat::getUsers();
+            break;
+
+        case 'getChats':
+            $response = Chat::getChats($_GET['lastID']);
+            break;
+
+        case 'admin':
+            $username = checkInput($_Post['name']);
+            $email = checkInput($_Post['email']);
+
+            $response = Chat::loginAdmin($username, $email);
+
+            //$response = Chat::loginAdmin($_POST['name'], $_POST['email']);
             break;
 
         case 'adminlogged':
@@ -82,24 +93,23 @@ try{
             break;
 
         case 'saveUser':
-			$response = Chat::saveUser($_POST['uID'], $_POST['status']);
+            $response = Chat::saveUser($_POST['uID'], $_POST['status']);
             break;
 
-		default:
-			throw new Exception('Wrong action');
-	}
-	
-	echo json_encode($response);
-}
-catch(Exception $e){
-	die(json_encode(array('error' => $e->getMessage())));
+        default:
+            throw new Exception('Wrong action');
+    }
+
+    echo json_encode($response);
+} catch (Exception $e) {
+    die(json_encode(array('error' => $e->getMessage())));
 }
 
-function checkInput ($chImput)
+function checkInput($chImput)
 {
-	$trim_input = trim($chImput); //Entfernt Whitespaces am Anfang und Ende eines Stringes
-	$trim_input = htmlspecialchars($trim_input, ENT_QUOTES); //Wanderlt Sonderzeichen in HTML-Codes um
-	return $trim_input;
+    $trim_input = trim($chImput); //Entfernt Whitespaces am Anfang und Ende eines Stringes
+    $trim_input = htmlspecialchars($trim_input, ENT_QUOTES); //Wanderlt Sonderzeichen in HTML-Codes um
+    return $trim_input;
 }
 
 ?>
